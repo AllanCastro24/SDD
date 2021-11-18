47a48b316f667b4ef351515494a075ec04200edc
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2021-11-17 21:32:55 -0700

    Se agregó lo el proyecto fuente

---
+++ naive-bayes/principal.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace naive_bayes
+{
+    partial class principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_salir = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Depth = 0;
+            this.btn_salir.Location = new System.Drawing.Point(767, 32);
+            this.btn_salir.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Primary = true;
+            this.btn_salir.Size = new System.Drawing.Size(21, 22);
+            this.btn_salir.TabIndex = 0;
+            this.btn_salir.Text = "X";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_salir);
+            this.Name = "principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clasificador Naive - Bayes";
+            this.Load += new System.EventHandler(this.principal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btn_salir;
+    }
+}
+
---
+++ naive-bayes/principal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace naive_bayes
+{
+    public partial class principal : MaterialSkin.Controls.MaterialForm
+    {
+        public principal()
+        {
+            InitializeComponent();
+        }
+
+        private void principal_Load(object sender, EventArgs e)
+        {
+            SkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            SkinManager.ColorScheme = new ColorScheme(Primary.LightBlue900, Primary.LightBlue900, Primary.LightGreen900, Accent.Green700, TextShade.WHITE);
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
