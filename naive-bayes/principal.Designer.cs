a4d9c3b2fe4feba3d6a0c3e0538e72a9a49b88a3
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2021-11-29 23:57:29 -0700

    Actualización de formulario

--- naive-bayes/principal.Designer.cs
+++ naive-bayes/principal.Designer.cs
@@ -30,39 +30,110 @@ namespace naive_bayes
         private void InitializeComponent()
         {
             this.btn_salir = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.txt_ruta_dataset = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btn_cargar_dataset = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
             // btn_salir
             // 
             this.btn_salir.Depth = 0;
-            this.btn_salir.Location = new System.Drawing.Point(1461, 31);
+            this.btn_salir.Location = new System.Drawing.Point(686, 32);
             this.btn_salir.MouseState = MaterialSkin.MouseState.HOVER;
             this.btn_salir.Name = "btn_salir";
             this.btn_salir.Primary = true;
-            this.btn_salir.Size = new System.Drawing.Size(21, 22);
+            this.btn_salir.Size = new System.Drawing.Size(26, 22);
             this.btn_salir.TabIndex = 0;
             this.btn_salir.Text = "X";
             this.btn_salir.UseVisualStyleBackColor = true;
             this.btn_salir.Click += new System.EventHandler(this.materialRaisedButton1_Click);
             // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(35, 117);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(159, 23);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "Cargar dataset";
+            // 
+            // txt_ruta_dataset
+            // 
+            this.txt_ruta_dataset.Depth = 0;
+            this.txt_ruta_dataset.Enabled = false;
+            this.txt_ruta_dataset.Hint = "";
+            this.txt_ruta_dataset.Location = new System.Drawing.Point(213, 117);
+            this.txt_ruta_dataset.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txt_ruta_dataset.Name = "txt_ruta_dataset";
+            this.txt_ruta_dataset.PasswordChar = '\0';
+            this.txt_ruta_dataset.SelectedText = "";
+            this.txt_ruta_dataset.SelectionLength = 0;
+            this.txt_ruta_dataset.SelectionStart = 0;
+            this.txt_ruta_dataset.Size = new System.Drawing.Size(333, 28);
+            this.txt_ruta_dataset.TabIndex = 2;
+            this.txt_ruta_dataset.UseSystemPasswordChar = false;
+            // 
+            // btn_cargar_dataset
+            // 
+            this.btn_cargar_dataset.Depth = 0;
+            this.btn_cargar_dataset.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cargar_dataset.Location = new System.Drawing.Point(578, 117);
+            this.btn_cargar_dataset.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_cargar_dataset.Name = "btn_cargar_dataset";
+            this.btn_cargar_dataset.Primary = true;
+            this.btn_cargar_dataset.Size = new System.Drawing.Size(56, 28);
+            this.btn_cargar_dataset.TabIndex = 3;
+            this.btn_cargar_dataset.Text = "...";
+            this.btn_cargar_dataset.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cargar_dataset.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(35, 211);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(92, 23);
+            this.materialLabel2.TabIndex = 4;
+            this.materialLabel2.Text = "Pruebas";
+            // 
             // principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1494, 864);
+            this.ClientSize = new System.Drawing.Size(724, 864);
             this.ControlBox = false;
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.btn_cargar_dataset);
+            this.Controls.Add(this.txt_ruta_dataset);
+            this.Controls.Add(this.materialLabel1);
             this.Controls.Add(this.btn_salir);
             this.Name = "principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Clasificador Naive - Bayes";
             this.Load += new System.EventHandler(this.principal_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private MaterialSkin.Controls.MaterialRaisedButton btn_salir;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txt_ruta_dataset;
+        private MaterialSkin.Controls.MaterialRaisedButton btn_cargar_dataset;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
     }
 }
 
