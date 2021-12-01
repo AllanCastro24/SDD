af1a42209891094b7975c06627577339ba1fbdc0
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2021-11-30 21:43:30 -0700

    Validación de checks en principal

--- naive-bayes/principal.Designer.cs
+++ naive-bayes/principal.Designer.cs
@@ -39,14 +39,14 @@ namespace naive_bayes
             this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
             this.txt_poracentaje_entrenamiento = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.rb_validacion_simple = new MaterialSkin.Controls.MaterialRadioButton();
-            this.rb_mismo_dataset = new MaterialSkin.Controls.MaterialRadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.materialRadioButton1 = new MaterialSkin.Controls.MaterialRadioButton();
-            this.txt_ruta_dataset_pruebas = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.btn_cargar_pruebas_externo = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.txt_ruta_dataset_pruebas = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
             this.txt_intervalo_discretizacion = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btn_analisis = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.cb_mismo_dataset = new MaterialSkin.Controls.MaterialCheckBox();
+            this.cb_archivo_externo = new MaterialSkin.Controls.MaterialCheckBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -125,11 +125,11 @@ namespace naive_bayes
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cb_mismo_dataset);
             this.groupBox1.Controls.Add(this.rb_validacion_cruzada);
             this.groupBox1.Controls.Add(this.materialLabel3);
             this.groupBox1.Controls.Add(this.txt_poracentaje_entrenamiento);
             this.groupBox1.Controls.Add(this.rb_validacion_simple);
-            this.groupBox1.Controls.Add(this.rb_mismo_dataset);
             this.groupBox1.Controls.Add(this.materialLabel2);
             this.groupBox1.Location = new System.Drawing.Point(39, 198);
             this.groupBox1.Name = "groupBox1";
@@ -198,52 +198,33 @@ namespace naive_bayes
             this.rb_validacion_simple.TabStop = true;
             this.rb_validacion_simple.Text = "Validación simple";
             this.rb_validacion_simple.UseVisualStyleBackColor = true;
-            // 
-            // rb_mismo_dataset
-            // 
-            this.rb_mismo_dataset.AutoSize = true;
-            this.rb_mismo_dataset.Depth = 0;
-            this.rb_mismo_dataset.Font = new System.Drawing.Font("Roboto", 10F);
-            this.rb_mismo_dataset.Location = new System.Drawing.Point(63, 58);
-            this.rb_mismo_dataset.Margin = new System.Windows.Forms.Padding(0);
-            this.rb_mismo_dataset.MouseLocation = new System.Drawing.Point(-1, -1);
-            this.rb_mismo_dataset.MouseState = MaterialSkin.MouseState.HOVER;
-            this.rb_mismo_dataset.Name = "rb_mismo_dataset";
-            this.rb_mismo_dataset.Ripple = true;
-            this.rb_mismo_dataset.Size = new System.Drawing.Size(145, 30);
-            this.rb_mismo_dataset.TabIndex = 6;
-            this.rb_mismo_dataset.TabStop = true;
-            this.rb_mismo_dataset.Text = "Mismo dataset";
-            this.rb_mismo_dataset.UseVisualStyleBackColor = true;
-            this.rb_mismo_dataset.CheckedChanged += new System.EventHandler(this.rb_mismo_dataset_CheckedChanged);
+            this.rb_validacion_simple.CheckedChanged += new System.EventHandler(this.rb_validacion_simple_CheckedChanged);
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.cb_archivo_externo);
             this.groupBox2.Controls.Add(this.btn_cargar_pruebas_externo);
             this.groupBox2.Controls.Add(this.txt_ruta_dataset_pruebas);
-            this.groupBox2.Controls.Add(this.materialRadioButton1);
             this.groupBox2.Location = new System.Drawing.Point(39, 476);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(635, 156);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             // 
-            // materialRadioButton1
+            // btn_cargar_pruebas_externo
             // 
-            this.materialRadioButton1.AutoSize = true;
-            this.materialRadioButton1.Depth = 0;
-            this.materialRadioButton1.Font = new System.Drawing.Font("Roboto", 10F);
-            this.materialRadioButton1.Location = new System.Drawing.Point(63, 28);
-            this.materialRadioButton1.Margin = new System.Windows.Forms.Padding(0);
-            this.materialRadioButton1.MouseLocation = new System.Drawing.Point(-1, -1);
-            this.materialRadioButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRadioButton1.Name = "materialRadioButton1";
-            this.materialRadioButton1.Ripple = true;
-            this.materialRadioButton1.Size = new System.Drawing.Size(241, 30);
-            this.materialRadioButton1.TabIndex = 0;
-            this.materialRadioButton1.TabStop = true;
-            this.materialRadioButton1.Text = "Archivo de pruebas externo";
-            this.materialRadioButton1.UseVisualStyleBackColor = true;
+            this.btn_cargar_pruebas_externo.Depth = 0;
+            this.btn_cargar_pruebas_externo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cargar_pruebas_externo.Location = new System.Drawing.Point(518, 85);
+            this.btn_cargar_pruebas_externo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_cargar_pruebas_externo.Name = "btn_cargar_pruebas_externo";
+            this.btn_cargar_pruebas_externo.Primary = true;
+            this.btn_cargar_pruebas_externo.Size = new System.Drawing.Size(56, 28);
+            this.btn_cargar_pruebas_externo.TabIndex = 4;
+            this.btn_cargar_pruebas_externo.Text = "...";
+            this.btn_cargar_pruebas_externo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_cargar_pruebas_externo.UseVisualStyleBackColor = true;
+            this.btn_cargar_pruebas_externo.Click += new System.EventHandler(this.btn_cargar_pruebas_externo_Click);
             // 
             // txt_ruta_dataset_pruebas
             // 
@@ -260,21 +241,6 @@ namespace naive_bayes
             this.txt_ruta_dataset_pruebas.Size = new System.Drawing.Size(373, 28);
             this.txt_ruta_dataset_pruebas.TabIndex = 3;
             this.txt_ruta_dataset_pruebas.UseSystemPasswordChar = false;
-            // 
-            // btn_cargar_pruebas_externo
-            // 
-            this.btn_cargar_pruebas_externo.Depth = 0;
-            this.btn_cargar_pruebas_externo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_cargar_pruebas_externo.Location = new System.Drawing.Point(518, 85);
-            this.btn_cargar_pruebas_externo.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btn_cargar_pruebas_externo.Name = "btn_cargar_pruebas_externo";
-            this.btn_cargar_pruebas_externo.Primary = true;
-            this.btn_cargar_pruebas_externo.Size = new System.Drawing.Size(56, 28);
-            this.btn_cargar_pruebas_externo.TabIndex = 4;
-            this.btn_cargar_pruebas_externo.Text = "...";
-            this.btn_cargar_pruebas_externo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btn_cargar_pruebas_externo.UseVisualStyleBackColor = true;
-            this.btn_cargar_pruebas_externo.Click += new System.EventHandler(this.btn_cargar_pruebas_externo_Click);
             // 
             // materialLabel5
             // 
@@ -305,25 +271,60 @@ namespace naive_bayes
             this.txt_intervalo_discretizacion.TabIndex = 9;
             this.txt_intervalo_discretizacion.UseSystemPasswordChar = false;
             // 
-            // materialRaisedButton1
+            // btn_analisis
             // 
-            this.materialRaisedButton1.Depth = 0;
-            this.materialRaisedButton1.Location = new System.Drawing.Point(490, 668);
-            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRaisedButton1.Name = "materialRaisedButton1";
-            this.materialRaisedButton1.Primary = true;
-            this.materialRaisedButton1.Size = new System.Drawing.Size(184, 54);
-            this.materialRaisedButton1.TabIndex = 10;
-            this.materialRaisedButton1.Text = "Análisis";
-            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.btn_analisis.Depth = 0;
+            this.btn_analisis.Location = new System.Drawing.Point(490, 668);
+            this.btn_analisis.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_analisis.Name = "btn_analisis";
+            this.btn_analisis.Primary = true;
+            this.btn_analisis.Size = new System.Drawing.Size(184, 54);
+            this.btn_analisis.TabIndex = 10;
+            this.btn_analisis.Text = "Análisis";
+            this.btn_analisis.UseVisualStyleBackColor = true;
+            this.btn_analisis.Click += new System.EventHandler(this.materialRaisedButton1_Click_1);
+            // 
+            // cb_mismo_dataset
+            // 
+            this.cb_mismo_dataset.AutoSize = true;
+            this.cb_mismo_dataset.Depth = 0;
+            this.cb_mismo_dataset.Font = new System.Drawing.Font("Roboto", 10F);
+            this.cb_mismo_dataset.Location = new System.Drawing.Point(63, 63);
+            this.cb_mismo_dataset.Margin = new System.Windows.Forms.Padding(0);
+            this.cb_mismo_dataset.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.cb_mismo_dataset.MouseState = MaterialSkin.MouseState.HOVER;
+            this.cb_mismo_dataset.Name = "cb_mismo_dataset";
+            this.cb_mismo_dataset.Ripple = true;
+            this.cb_mismo_dataset.Size = new System.Drawing.Size(151, 30);
+            this.cb_mismo_dataset.TabIndex = 11;
+            this.cb_mismo_dataset.Text = "Mismo dataset:";
+            this.cb_mismo_dataset.UseVisualStyleBackColor = true;
+            this.cb_mismo_dataset.CheckedChanged += new System.EventHandler(this.cb_mismo_dataset_CheckedChanged);
+            // 
+            // cb_archivo_externo
+            // 
+            this.cb_archivo_externo.AutoSize = true;
+            this.cb_archivo_externo.Depth = 0;
+            this.cb_archivo_externo.Font = new System.Drawing.Font("Roboto", 10F);
+            this.cb_archivo_externo.Location = new System.Drawing.Point(63, 18);
+            this.cb_archivo_externo.Margin = new System.Windows.Forms.Padding(0);
+            this.cb_archivo_externo.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.cb_archivo_externo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.cb_archivo_externo.Name = "cb_archivo_externo";
+            this.cb_archivo_externo.Ripple = true;
+            this.cb_archivo_externo.Size = new System.Drawing.Size(246, 30);
+            this.cb_archivo_externo.TabIndex = 12;
+            this.cb_archivo_externo.Text = "Archivo de pruebas externo:";
+            this.cb_archivo_externo.UseVisualStyleBackColor = true;
+            this.cb_archivo_externo.CheckedChanged += new System.EventHandler(this.cb_archivo_externo_CheckedChanged);
             // 
             // principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 763);
+            this.ClientSize = new System.Drawing.Size(727, 763);
             this.ControlBox = false;
-            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.btn_analisis);
             this.Controls.Add(this.txt_intervalo_discretizacion);
             this.Controls.Add(this.materialLabel5);
             this.Controls.Add(this.groupBox2);
@@ -353,18 +354,18 @@ namespace naive_bayes
         private MaterialSkin.Controls.MaterialRaisedButton btn_cargar_dataset;
         private MaterialSkin.Controls.MaterialLabel materialLabel2;
         private System.Windows.Forms.GroupBox groupBox1;
-        private MaterialSkin.Controls.MaterialRadioButton rb_mismo_dataset;
         private MaterialSkin.Controls.MaterialRadioButton rb_validacion_simple;
         private MaterialSkin.Controls.MaterialLabel materialLabel3;
         private MaterialSkin.Controls.MaterialSingleLineTextField txt_poracentaje_entrenamiento;
         private MaterialSkin.Controls.MaterialRadioButton rb_validacion_cruzada;
         private System.Windows.Forms.GroupBox groupBox2;
-        private MaterialSkin.Controls.MaterialRadioButton materialRadioButton1;
         private MaterialSkin.Controls.MaterialRaisedButton btn_cargar_pruebas_externo;
         private MaterialSkin.Controls.MaterialSingleLineTextField txt_ruta_dataset_pruebas;
         private MaterialSkin.Controls.MaterialLabel materialLabel5;
         private MaterialSkin.Controls.MaterialSingleLineTextField txt_intervalo_discretizacion;
-        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialRaisedButton btn_analisis;
+        private MaterialSkin.Controls.MaterialCheckBox cb_mismo_dataset;
+        private MaterialSkin.Controls.MaterialCheckBox cb_archivo_externo;
     }
 }
 
--- naive-bayes/principal.cs
+++ naive-bayes/principal.cs
@@ -60,38 +60,71 @@ namespace naive_bayes
             txt_ruta_dataset.Text = filePath;
         }
 
-        private void rb_mismo_dataset_CheckedChanged(object sender, EventArgs e)
+        private void btn_cargar_pruebas_externo_Click(object sender, EventArgs e)
+        {
+            if (cb_archivo_externo.Checked) { 
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\archivo";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Almacenar la ruta de un archivo especifico
+                        filePath = openFileDialog.FileName;
+
+                        //Leer el contenido del documento con un stream
+                        var fileStream = openFileDialog.OpenFile();
+
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                    }
+                }
+                txt_ruta_dataset_pruebas.Text = filePath;
+            }
+            else
+            {
+                MessageBox.Show("No indicó check de pruebas externo...","Mensaje del sistema");
+            }
+        }
+
+        private void cb_mismo_dataset_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_archivo_externo.Checked && cb_mismo_dataset.Checked)
+            {
+                MessageBox.Show("No puede indicar los dos checks", "Mensaje del sistema");
+
+                cb_archivo_externo.Checked = false;
+                cb_mismo_dataset.Checked = false;
+            }
+        }
+
+        private void cb_archivo_externo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_archivo_externo.Checked && cb_mismo_dataset.Checked)
+            {
+                MessageBox.Show("No puede indicar los dos checks", "Mensaje del sistema");
+
+                cb_archivo_externo.Checked = false;
+                cb_mismo_dataset.Checked = false;
+            }
+        }
+
+        private void materialRaisedButton1_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        private void btn_cargar_pruebas_externo_Click(object sender, EventArgs e)
+        private void rb_validacion_simple_CheckedChanged(object sender, EventArgs e)
         {
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
-
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.InitialDirectory = "c:\\archivo";
-                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //Almacenar la ruta de un archivo especifico
-                    filePath = openFileDialog.FileName;
-
-                    //Leer el contenido del documento con un stream
-                    var fileStream = openFileDialog.OpenFile();
-
-                    using (StreamReader reader = new StreamReader(fileStream))
-                    {
-                        fileContent = reader.ReadToEnd();
-                    }
-                }
-            }
-            txt_ruta_dataset_pruebas.Text = filePath;
+            
         }
     }
 }
